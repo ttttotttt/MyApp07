d61b47d42241ce298c625429b55afc21bcb416f6
Suzuno Tomohiro <ezorisu.snow@gmail.com>
2020-10-28 17:04:27 +0900

    テスト

--- app/src/main/java/com/example/myapp07/acessIntentOfRecognizer.java
+++ app/src/main/java/com/example/myapp07/acessIntentOfRecognizer.java
@@ -3,7 +3,8 @@ package com.example.myapp07;
 //import android.content.ComponentName;
 //import android.content.Context;
 //import 	android.speech.SpeechRecognizer;
-//
+
+//ライブラリは継承できないのか？要確認
 //public class acessIntentOfRecognizer extends  SpeechRecognizer {
 //    public acessIntentOfRecognizer() {
 //
